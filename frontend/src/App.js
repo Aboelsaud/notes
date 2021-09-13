219bb0370b16c76578b9a63c580a072fc5a3e945
Aboelsaoud <yousefaboalaoud@gmail.com>
2021-09-13 21:33:38 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import {
+  HashRouter as Router,
+  Route
+} from "react-router-dom";
+
+
+import './App.css';
+import Header from './components/header'
+import NotesListPage from './pages/NotesListPage'
+import NotePage from './pages/NotePage'
+
+function App() {
+  return (
+    <Router>
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          <Route path="/" exact component={NotesListPage} />
+          <Route path="/note/:id" component={NotePage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
